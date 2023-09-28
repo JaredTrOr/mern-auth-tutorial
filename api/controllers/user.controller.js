888a35a1bd4ebd2a10bd7849d72d9aa25ccc1699
JaredTrOr <trujillojaredalexander@gmail.com>
2023-09-28 01:19:58 -0600

    create test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,7 @@
+export class UserController {
+
+    static userRoot (req,res) {
+        res.json({message: 'User API route'})
+    }
+
+}
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
 dotenv.config()
 
 //MongoDB connection
@@ -11,3 +12,7 @@ mongoose.connect(process.env.DBMONGOCONNECTION)
 //Creation of the express server
 const app = express()
 app.listen(process.env.PORT, () => console.log('Server listening on port 3000'))
+
+//Routes
+app.get('/', (req,res) => res.json({message: 'MERN-AUTH server online'}))
+app.use('/api/user', userRouter)
